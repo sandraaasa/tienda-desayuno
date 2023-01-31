5593c3ee6cb41485b42f60ce3d48b08fbd90995f
sandra <sandra_saanchez@hotmail.com>
2023-01-31 16:57:58 +0100

    dfhd

--- js/compra.js
+++ js/compra.js
@@ -1,4 +1,12 @@
-window.addEventListener("load",inicia);
-function inicia() {
+
+window.onload = cargar;
+function cargar(){
     
+    fetch("datos.json")
+        .then (respuesta => respuesta.json())
+        .then (json => cargarJSON(json))
+        .catch(e => alert(`Error: ${e}`));
 }
+function cargarJSON(json){   
+    document.getElementById("nombre").innerHTML+=json["@usuarios"][0].user;
+} 
