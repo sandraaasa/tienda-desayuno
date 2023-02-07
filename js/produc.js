8e2fc27d188f092ba4fb0e5c337466ced15e122d
sandra200210 <sandra_saanchez@hotmail.com>
2023-02-07 10:33:53 +0100

    guarda guarda

--- js/produc.js
+++ js/produc.js
@@ -21,6 +21,66 @@ window.onload=function () {
             
         });
     }
-    
+    cargar();
 
+}
+function cargar() {
+    cargarPelicula()
+    .then ((json) => cargarJSON(json))
+    .catch((e) => alert(`Error: ${e}`));
+}
+
+async function cargarPelicula(){
+    const url = "datos.json";
+    const respuesta = await fetch (url)  //espero a que fetch me devuelva la respuesta
+    const json = await respuesta.json() // espero a que se ejecute el método json()
+    return json
+}
+{/* <div class="col">
+        <div class="card shadow-sm">
+        <img src="../img/b-frutas.jpg" alt="batidos" width="100%" >height="225" 
+
+            <div class="card-body">
+                <h5 class="card-tittle">Ingredientes</h5>
+                <div class="card-text">
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary fav"><img src="../img/svg/heart.svg" alt="fav"></button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary buy"> <img src="../img/svg/bag-plus.svg" alt="buy"></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> */}
+function cargarJSON( json ) {
+    let content = document.getElementById("contenedor");
+    
+    for (let i = 0; i < json.length; i++) {
+        content.innerHTML+='<div class="col">'
+                           + '<div class="card shadow-sm">';
+        content.innerHTML+='<img src="../'+json[i].img+'" alt="'+json[i].id+'" width="100%" >';
+        content.innerHTML+='<div class="card-body">'
+                            +'<h5 class="card-tittle">Ingredientes</h5>'
+                            +'<div class="card-text">'
+                            +'<ul>';
+        for (let j = 0; j < json[i].ingredientes.length; j++) {
+            content.innerHTML+="<li>"+json[i].ingredientes[j]+"</li>";
+        }
+        content.innerHTML+='</ul> </div>'
+                            +'<div class="d-flex justify-content-between align-items-center">'
+                                +'<div class="btn-group">'
+                                    +'<button type="button" class="btn btn-sm btn-outline-secondary fav"><img src="../img/svg/heart.svg" alt="fav"></button>'
+                                    +'<button type="button" class="btn btn-sm btn-outline-secondary buy"> <img src="../img/svg/bag-plus.svg" alt="buy"></button>'
+                                +'</div>'
+                            +'</div>'
+                            +'</div></div></div>';
+
+
+        
+        
+    }
 }
